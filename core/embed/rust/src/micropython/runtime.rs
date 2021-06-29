58cafd0d5a5aeddc48389e9f030e69264c8cf4e8
Jan Pochyla <jpochyla@gmail.com>
2021-06-29 19:34:47 +0200

    chore(core): Add test for Rust exc catching

--- core/embed/rust/src/micropython/runtime.rs
+++ core/embed/rust/src/micropython/runtime.rs
@@ -11,7 +11,7 @@ pub fn raise_value_error(msg: &'static CStr) -> ! {
     panic!();
 }
 
-pub fn call_except<F, T>(mut func: F) -> Result<T, Obj>
+pub fn except<F, T>(mut func: F) -> Result<T, Obj>
 where
     F: FnMut() -> T,
 {
@@ -47,5 +47,24 @@ where
     let func = arg as *mut F;
     unsafe {
         (*func)();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn except_returns_ok_on_no_exception() {
+        let result = except(|| 1);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 1);
+    }
+
+    #[test]
+    fn except_catches_value_error() {
+        let msg = unsafe { CStr::from_bytes_with_nul_unchecked(b"msg\0") };
+        let result = except(|| raise_value_error(&msg));
+        assert!(result.is_err());
     }
 }
