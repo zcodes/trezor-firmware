87305ee71a41ec498dd5b448d5b2ec82a4d77fd0
Jan Pochyla <jpochyla@gmail.com>
2021-07-27 10:17:26 +0200

    chore(core): Document exception catching in Rust

--- core/embed/rust/src/micropython/runtime.rs
+++ core/embed/rust/src/micropython/runtime.rs
@@ -11,15 +11,27 @@ pub fn raise_value_error(msg: &'static CStr) -> ! {
     panic!();
 }
 
+/// Execute `func` while catching MicroPython exceptions. Returns `Ok` in the
+/// successful case, and `Err` with the caught `Obj` in case of a raise.
 pub fn except<F, T>(mut func: F) -> Result<T, Obj>
 where
     F: FnMut() -> T,
 {
+    // Because MicroPython exceptions use `setjmp` and `longjmp`-like mechanism that
+    // doesn't play too well with Rust, we setup the non-local return pads in C, and
+    // execute `func` through a callback.
+
     unsafe {
+        // First, we craft a wrapping closure that calls `func`. Because we are generic
+        // over the return type, we cannot pass the returned value over the FFI
+        // boundary, so we assign it explicitly in `wrapper`.
         let mut result = MaybeUninit::zeroed();
         let mut wrapper = || {
             result = MaybeUninit::new(func());
         };
+        // `wrapper` is a closure, and to pass it over the FFI, we split it into a function
+        // pointer, and a user-data pointer. `ffi::trezor_obj_call_protected` then calls
+        // the `callback` with the `argument`.
         let (callback, argument) = split_func_into_callback_and_argument(&mut wrapper);
         let exception = ffi::trezor_obj_call_protected(Some(callback), argument);
         if exception == Obj::const_none() {
@@ -37,6 +49,9 @@ fn split_func_into_callback_and_argument<F>(func: &mut F) -> (ProtectedCallback,
 where
     F: FnMut(),
 {
+    // Here we mono-morphize a version of `trampoline` for each type `F`, so it
+    // calls the correct `FnMut` impl, and cast `func` into its data part to use
+    // as the argument.
     (trampoline::<F>, func as *mut _ as *mut _)
 }
 
@@ -44,6 +59,8 @@ unsafe extern "C" fn trampoline<F>(arg: ProtectedArgument)
 where
     F: FnMut(),
 {
+    // Synthesize a callable `*mut F` from the closure environment pointer `arg`,
+    // and call it.
     let func = arg as *mut F;
     unsafe {
         (*func)();
